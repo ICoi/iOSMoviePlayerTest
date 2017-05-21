63240ab6d16723ab3f57592a952d2e9b9e8d046a
Joung Daun <ICo1022@gmail.com>
2017-05-21 22:37:05 +0900

    Show Left time and Video Duration

--- ICoMoviePlayerTest/ICoMoviePlayerTest/AVPlayerLayerViewController.m
+++ ICoMoviePlayerTest/ICoMoviePlayerTest/AVPlayerLayerViewController.m
@@ -12,6 +12,8 @@
 #define MOVIE_URL @"http://image-aka.qpyou.cn/prvideo/test/360p/15s_summon.mp4"
 
 @interface AVPlayerLayerViewController ()
+@property (strong, nonatomic) UITextView *leftTimeTV;
+@property  CMTime movieDuration;
 
 @end
 
@@ -39,9 +41,24 @@
 */
 
 - (void)showVideo{
+    
+    
+    //    AVAsset *avAsset = [AVAsset assetWithURL:url];
+    //    AVPlayerItem* avPlayerItem =[[AVPlayerItem alloc]initWithAsset:avAsset];
+    //    AVPlayer* avPlayer = [[AVPlayer alloc]initWithPlayerItem:avPlayerItem];
+    //    AVPlayerLayer *avPlayerLayer =[AVPlayerLayer playerLayerWithPlayer:avPlayer];
+    //    [avPlayerLayer setFrame:self.view.frame];
+    //    [self.view.layer addSublayer:avPlayerLayer];
+    //    //[avPlayerLayer setBackgroundColor:[[UIColor redColor]CGColor]];
+    //    [avPlayer seekToTime:kCMTimeZero];
+    //    [avPlayer play];
+
+    
     NSURL *url = [NSURL URLWithString:MOVIE_URL];
     
     UIView *playerView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 320, 480)];
+    
+  
     playerView.backgroundColor = [UIColor blackColor];
     {
         AVPlayerItem *playerItem = [AVPlayerItem playerItemWithURL:url];
@@ -56,6 +73,20 @@
             
             [avPlayer play];
         }
+        
+        {
+            self.movieDuration = avPlayer.currentItem.asset.duration;
+            
+            self.leftTimeTV = [[UITextView alloc] initWithFrame:CGRectMake(0, 0, 100, 100)];
+            self.leftTimeTV.text = @"";
+            [self.view addSubview:self.leftTimeTV];
+            
+            [avPlayer addPeriodicTimeObserverForInterval:CMTimeMake(1.0, 6.0) queue:dispatch_get_main_queue() usingBlock:^(CMTime time) {
+                // TODO
+                [self.leftTimeTV setText:[NSString stringWithFormat:@"%f / %f", CMTimeGetSeconds(time) , CMTimeGetSeconds(self.movieDuration)]];
+            }];
+        }
+        
         avPlayer.actionAtItemEnd = AVPlayerActionAtItemEndNone;
         [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(playerItemDidReachEnd:) name:AVPlayerItemDidPlayToEndTimeNotification object:[avPlayer currentItem]];
     }
--- ICoMoviePlayerTest/ICoMoviePlayerTest/AVPlayerTestViewController.m
+++ ICoMoviePlayerTest/ICoMoviePlayerTest/AVPlayerTestViewController.m
@@ -18,7 +18,8 @@
 
 #define MOVIE_URL @"http://image-aka.qpyou.cn/prvideo/test/360p/15s_summon.mp4"
 @interface AVPlayerTestViewController ()
-
+@property (strong, nonatomic) UITextView *leftTimeTV;
+@property  CMTime movieDuration;
 @end
 
 @implementation AVPlayerTestViewController
@@ -52,33 +53,42 @@
 //    
 //    // 방법 1.
 //    //create an AVPlayer
-//    AVPlayer *player = [AVPlayer playerWithURL:url];
-//    
-//    // create a player view controller
-//    AVPlayerViewController *controller = [[AVPlayerViewController alloc] init];
-//    controller.player = player;
-//    [player play];
-//    
-//    // show the view Controller
-//    [self addChildViewController:controller];
-//    [self.view addSubview:controller.view];
-//    controller.view.frame = self.view.frame;
+    AVPlayer *player = [AVPlayer playerWithURL:url];
+    
+    // create a player view controller
+    AVPlayerViewController *controller = [[AVPlayerViewController alloc] init];
+    controller.showsPlaybackControls = false;
+    controller.player = player;
+    [player play];
+    
+    // show the view Controller
+    [self addChildViewController:controller];
+    [self.view addSubview:controller.view];
+    controller.view.frame = self.view.frame;
+    
+    self.movieDuration = player.currentItem.asset.duration;
     
     
+    self.leftTimeTV = [[UITextView alloc] initWithFrame:CGRectMake(0, 0, 100, 100)];
+    self.leftTimeTV.text = @"";
+    [controller.view addSubview:self.leftTimeTV];
     
-    // 방법 2. 이건 배경이 흰색으로.. 재생바 없이 나옴.. 왜?
-    AVAsset *avAsset = [AVAsset assetWithURL:url];
-    AVPlayerItem* avPlayerItem =[[AVPlayerItem alloc]initWithAsset:avAsset];
-    AVPlayer* avPlayer = [[AVPlayer alloc]initWithPlayerItem:avPlayerItem];
-    AVPlayerLayer *avPlayerLayer =[AVPlayerLayer playerLayerWithPlayer:avPlayer];
-    [avPlayerLayer setFrame:self.view.frame];
-    [self.view.layer addSublayer:avPlayerLayer];
-    //[avPlayerLayer setBackgroundColor:[[UIColor redColor]CGColor]];
-    [avPlayer seekToTime:kCMTimeZero];
-    [avPlayer play];
     
-    // key value observer등록
-    [avPlayerItem addObserver:<#(nonnull NSObject *)#> forKeyPath:<#(nonnull NSString *)#> options:<#(NSKeyValueObservingOptions)#> context:<#(nullable void *)#>
+    [player.currentItem.asset addObserver:self forKeyPath:@"duration" options:NSKeyValueObservingOptionNew context:@"12333"];
+    
+    // About CMTimeMake https://devpassya.wordpress.com/2012/09/26/cmtime-에-대한-이해/
+    [player addPeriodicTimeObserverForInterval:CMTimeMake(1.0, 6.0) queue:dispatch_get_main_queue() usingBlock:^(CMTime time) {
+        // TODO
+//        NSLog(@"aaa %f", CMTimeGetSeconds(time));
+        [self.leftTimeTV setText:[NSString stringWithFormat:@"%f / %f", CMTimeGetSeconds(time) , CMTimeGetSeconds(self.movieDuration)]];
+    }];
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(playerItemDidReachEnd:) name:AVPlayerItemDidPlayToEndTimeNotification object:[player currentItem]];
 }
 
+
+- (void)playerItemDidReachEnd:(NSNotification *)notification {
+    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Video END" message:@"Video Play END" delegate:nil cancelButtonTitle:@"ok" otherButtonTitles: nil];
+    [alert show];
+}
 @end
